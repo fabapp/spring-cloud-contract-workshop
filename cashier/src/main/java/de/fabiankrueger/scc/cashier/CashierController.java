87083d81e6675a4978ecd046c2c0acffcf59bdc4
fabapp2 <fkrueger@pivotal.io>
2020-09-17 15:33:59 +0200

    commented out business logic

--- cashier/src/main/java/de/fabiankrueger/scc/cashier/CashierController.java
+++ cashier/src/main/java/de/fabiankrueger/scc/cashier/CashierController.java
@@ -2,12 +2,9 @@ package de.fabiankrueger.scc.cashier;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,9 +18,11 @@ public class CashierController {
         return ResponseEntity.ok(processedOrder);
     }
 
+    /*
     @PostMapping("/order/{orderId}/payment")
     public ResponseEntity<Payment> processPayment(@RequestBody Map<String, Double> payment, @PathVariable Long orderId) {
         final Payment processedPayment = cashierService.processPayment(orderId, payment.get("amountGiven"));
         return ResponseEntity.ok(processedPayment);
     }
+    */
 }
