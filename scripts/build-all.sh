c3a5a6222eabb343eace908f2965295b72bea842
Fabian Krüger <fkrueger@vmware.com>
2020-10-30 00:42:29 +0100

    cleaned up

--- scripts/build-all.sh
+++ scripts/build-all.sh
@@ -1,13 +1,14 @@
+cd ..
 echo "create docker container for customer"
 cd ./customer
 ./mvnw clean package spring-boot:build-image
-cd ..
 
+cd ..
 echo "create docker container for cashier"
 cd ./cashier
 ./mvnw clean package  spring-boot:build-image
-cd ..
 
+cd ..
 echo "create docker container for barista"
 cd ./barista
 ./mvnw clean package spring-boot:build-image
--- scripts/stub-runner-server.sh
+++ scripts/stub-runner-server.sh
@@ -1,3 +1,6 @@
+stubrunnerpath=~/git/spring-cloud-contract-workshop/exercise/stub-runner
+# DOWNLOADED
+java -jar ${stubrunnerpath}/stub-runner.jar  --server.port=8085 --stubrunner.stubsMode="LOCAL" --stubrunner.ids="de.fabiankrueger.scc:cashier:1.0-SNAPSHOT:stubs:8083"
 
 # REMOTE
 #java -jar ../stub-runner-server/target/stub-runner-server-1.0-SNAPSHOT.jar \
@@ -6,6 +9,6 @@
 #    --stubrunner.repositoryRoot="git://file:///Users/fkrueger/git/spring-cloud-contract-workshop/exercise/contracts-repo/"
 
 # LOCAL
-java -jar ../stub-runner-server/target/stub-runner-server-1.0-SNAPSHOT.jar \
-    --stubrunner.stubsMode="LOCAL" \
-    --stubrunner.ids="de.fabiankrueger.scc:cashier:1.0-SNAPSHOT:stubs:8083"
+#java -jar ../stub-runner-server/target/stub-runner-server-1.0-SNAPSHOT.jar \
+#    --stubrunner.stubsMode="LOCAL" \
+#    --stubrunner.ids="de.fabiankrueger.scc:cashier:1.0-SNAPSHOT:stubs:8083"
